1d3eac2536f5efb02fe21e36d3e4020db21d1200
Aykut Mürkit <ayktmrkt@gmail.com>
2024-08-05 22:18:12 +0300

    useEffect

--- 5.useEffect/src/App.jsx
+++ 5.useEffect/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  useEffect(() => {
+    console.log("her zaman çalışır");
+  });
+
+  useEffect(() => {
+    console.log("sadece başlangıçta çalışır");
+  }, []);
+
+  useEffect(() => {
+    console.log("başlangıçta veya firstname değiştiğinde çalışır");
+  }, [firstName]);
+
+  useEffect(() => {
+    console.log(
+      "başlangıçta veya firstname veya lastname değiştiğinde çalışır"
+    );
+  }, [firstName, lastName]);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={() => setFirstName("aykut")}>ismi değiştir</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <button onClick={() => setLastName("mürkit")}>adı değiştir</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
