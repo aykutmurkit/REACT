1f997d564a565fd47de6fcf3a10cfb86db786dfc
aykutmurkit <ayktmrkt@gmail.com>
2024-08-07 14:55:10 +0300

    useparams ve usenavigate
    bitirmedim

---
+++ 13.router-dom/src/pages/Product.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function Product({ product }) {
+  const { id, name, price } = product;
+  const navigate = useNavigate();
+
+  return (
+    <div style={{ marginBottom:"40px", backgroundColor:"lightgray"}}>
+      <div>Ürün Detayı</div>
+      <h3>İSİM: {name}</h3>
+      <h3>FİYAT: {price}</h3>
+    <button onClick={()=>navigate("/product-details/"+id)}>detayına git</button>
+    </div>
+  );
+}
+
+export default Product;
--- 13.router-dom/src/pages/Products.jsx
+++ 13.router-dom/src/pages/Products.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Product from './Product';
 
 function Products() {
+  const products = [
+    {
+      id: "1",
+      name: "Bilgisayar",
+      price: 50000,
+    },
+    {
+      id: "2",
+      name: "Telefon",
+      price: 40000,
+    }
+  ];
+
   return (
-    <div>Products</div>
-  )
+    <div>
+      {
+        products && products.map((product) => (
+          <Product key={product.id} product={product} />
+        ))
+      }
+    </div>
+  );
 }
 
-export default Products
+export default Products;
